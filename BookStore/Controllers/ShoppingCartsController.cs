d65499769b2b34b01b2914f5d70a6e56e394fc48
Nikola Trajkovski <ntrajkovski5@gmail.com>
2024-09-09 01:52:07 +0200

    added shoppingCart controller

---
+++ BookStore/Controllers/ShoppingCartsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Data;
+using Domain.Models;
+using Service.Interface;
+using System.Security.Claims;
+
+namespace BookStore.Controllers
+{
+    public class ShoppingCartsController : Controller
+    {
+        private readonly IShoppingCartService _shoppingCartService;
+
+        public ShoppingCartsController(IShoppingCartService shoppingCartService)
+        {
+            _shoppingCartService = shoppingCartService;
+        }
+
+        // GET: ShoppingCarts
+        public IActionResult Index()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var dto = _shoppingCartService.getShoppingCartInfo(userId);
+            return View(dto);
+        }
+
+        public IActionResult DeleteFromShoppingCart(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            _shoppingCartService.deleteBookFromShoppingCart(userId, id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Order()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = _shoppingCartService.order(userId);
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -48,7 +48,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Books}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
